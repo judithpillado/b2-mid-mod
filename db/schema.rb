2d32db0d9b12a95201a51ee0a34c42d09140132f
Judith Pillado <judith.pillado@gmail.com>
2020-07-17 11:16:53 -0500

    Create Ride Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200717155752) do
+ActiveRecord::Schema.define(version: 20200717161557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20200717155752) do
   create_table "amusement_parks", force: :cascade do |t|
     t.string "name"
     t.integer "addmission_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rides", force: :cascade do |t|
+    t.string "name"
+    t.integer "thrill_rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
